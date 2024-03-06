ec2bab2c02d48363a3457b9604ff8b674e0a8525
Devanshu2343 <118683912+Devanshu2343@users.noreply.github.com>
2024-03-06 19:58:13 +0530

    Create array1.js
    array in JS and use of trim

---
+++ Array/array1.js
@@ -0,0 +1,6 @@
+let fruits = [ 1, "     ban    ana     " , "orange"];
+console.log(fruits[0]);
+//front space is not ignored 
+console.log(fruits[1]);
+//front space is ignored
+console.log(fruits[1].trim());
