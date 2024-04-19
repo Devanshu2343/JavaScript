264f31b2ac5b5ecc072182dc73ccda722ea5c206
Devanshu2343 <118683912+Devanshu2343@users.noreply.github.com>
2024-04-19 20:25:06 +0530

    Create object_return.js

---
+++ object_return.js
@@ -0,0 +1,8 @@
+function greet(){
+    return "Welcome";
+}
+var newGreet = greet();
+//to display function
+console.log(newGreet);
+// to check number of parameter in function greet
+console.log(greet.length);
